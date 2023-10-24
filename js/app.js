94965f1d88bd9c3ae66ef1d532be2fcd1cbf49cc
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-10-24 12:08:13 -0300

    se realiza el maquetado del ejercicio 9 y se comprueba su funcionamiento, ademas se cambia los var por let en las variables del codigo de app.js

--- js/app.js
+++ js/app.js
@@ -1,48 +1,48 @@
 function ejercicio1() {
   alert("Un Mensaje");
 }
-var punto1 = document.getElementById("btnPunto1");
+let punto1 = document.getElementById("btnPunto1");
 punto1.addEventListener("click", ejercicio1);
 
 function ejercicio2() {
-  var mensajePunto2 = document.getElementById("mensajePunto2");
+  let mensajePunto2 = document.getElementById("mensajePunto2");
   mensajePunto2.textContent = "Hello World!";
 }
-var punto2 = document.getElementById("btnPunto2");
+let punto2 = document.getElementById("btnPunto2");
 punto2.addEventListener("click", ejercicio2);
 
 function ejercicio3() {
-  var mensajePunto3 = document.getElementById("mensajePunto3");
+  let mensajePunto3 = document.getElementById("mensajePunto3");
   const num1 = 3;
   const num2 = 5;
-  var suma = num1 + num2;
+  let suma = num1 + num2;
   mensajePunto3.textContent = `3 + 5 = ${suma}`;
 }
-var punto3 = document.getElementById("btnPunto3");
+let punto3 = document.getElementById("btnPunto3");
 punto3.addEventListener("click", ejercicio3);
 
 function ejercicio4() {
-  var mensajePunto4 = document.getElementById("mensajePunto4");
-  var nombreUsuario = prompt("Ingrese El Nombre de Usuario");
+  let mensajePunto4 = document.getElementById("mensajePunto4");
+  let nombreUsuario = prompt("Ingrese El Nombre de Usuario");
   mensajePunto4.textContent = `Hola ${nombreUsuario}`;
 }
-var punto4 = document.getElementById("btnPunto4");
+let punto4 = document.getElementById("btnPunto4");
 punto4.addEventListener("click", ejercicio4);
 
 function ejercicio5() {
-  var mensajePunto5 = document.getElementById("mensajePunto5");
-  var num1 = parseInt(prompt("Ingrese el Valor del Numero 1:"));
-  var num2 = parseInt(prompt("Ingrese el valor del Numero 2:"));
-  var suma = num1 + num2;
+  let mensajePunto5 = document.getElementById("mensajePunto5");
+  let num1 = parseInt(prompt("Ingrese el Valor del Numero 1:"));
+  let num2 = parseInt(prompt("Ingrese el valor del Numero 2:"));
+  let suma = num1 + num2;
   mensajePunto5.textContent = `${num1} + ${num2} = ${suma}`;
 }
-var punto5 = document.getElementById("btnPunto5");
+let punto5 = document.getElementById("btnPunto5");
 punto5.addEventListener("click", ejercicio5);
 
 function ejercicio6() {
-  var mensajePunto6 = document.getElementById("mensajePunto6");
-  var num1 = parseInt(prompt("Ingrese el Valor Del Numero 1:"));
-  var num2 = parseInt(prompt("Ingrese el Valor Del Numero 2:"));
+  let mensajePunto6 = document.getElementById("mensajePunto6");
+  let num1 = parseInt(prompt("Ingrese el Valor Del Numero 1:"));
+  let num2 = parseInt(prompt("Ingrese el Valor Del Numero 2:"));
   if (num1 > num2) {
     mensajePunto6.textContent = `El primer numero(${num1}) es mayor que el segundo(${num2})`;
   } else if (num2 > num1) {
@@ -51,14 +51,14 @@ function ejercicio6() {
     mensajePunto6.textContent = `Ambos numeros comparten el mismo valor(${num1})`;
   }
 }
-var punto6 = document.getElementById("btnPunto6");
+let punto6 = document.getElementById("btnPunto6");
 punto6.addEventListener("click", ejercicio6);
 
 function ejercicio7() {
-  var mensajePunto7 = document.getElementById("mensajePunto7");
-  var num1 = parseInt(prompt("Ingrese el Valor Del Numero 1:"));
-  var num2 = parseInt(prompt("Ingrese el Valor Del Numero 2:"));
-  var num3 = parseInt(prompt("Ingrese el Valor Del Numero 3:"));
+  let mensajePunto7 = document.getElementById("mensajePunto7");
+  let num1 = parseInt(prompt("Ingrese el Valor Del Numero 1:"));
+  let num2 = parseInt(prompt("Ingrese el Valor Del Numero 2:"));
+  let num3 = parseInt(prompt("Ingrese el Valor Del Numero 3:"));
   if ((num1 > num2 && num1 > num3) || (num1 > num2 && num2 == num3)) {
     mensajePunto7.textContent = `El mayor de los 3 numeros ingresados es el primero(${num1})`;
   } else if ((num2 > num1 && num2 > num3) || (num2 > num1 && num1 == num3)) {
@@ -69,17 +69,36 @@ function ejercicio7() {
     mensajePunto7.textContent = `Los 3 valores son iguales`;
   }
 }
-var punto7 = document.getElementById("btnPunto7");
+let punto7 = document.getElementById("btnPunto7");
 punto7.addEventListener("click", ejercicio7);
 
 function ejercicio8() {
-  var mensajePunto8 = document.getElementById("mensajePunto8");
-  var num = parseInt(prompt("Ingrese el valor del Numero:"));
+  let mensajePunto8 = document.getElementById("mensajePunto8");
+  let num = parseInt(prompt("Ingrese el valor del Numero:"));
   if (num % 2 === 0) {
     mensajePunto8.textContent = `El numero ${num} es divisible en 2`;
   } else {
     mensajePunto8.textContent = `El numero ${num} no es divisible en 2`;
   }
 }
-var punto8 = document.getElementById("btnPunto8");
+let punto8 = document.getElementById("btnPunto8");
 punto8.addEventListener("click", ejercicio8);
+
+function ejercicio9() {
+  let mensajePunto9 = document.getElementById("mensajePunto9");
+  let frase = prompt("Ingrese la frase");
+
+  function vocales(letra) {
+    return "aeiouAEIOU".includes(letra);
+  }
+  let vocalesEncontradas = "";
+  for (let i = 0; i < frase.length; i++) {
+    const letra = frase.charAt(i);
+    if (vocales(letra)) {
+      vocalesEncontradas += letra;
+    }
+  }
+  mensajePunto9.textContent = `${vocalesEncontradas}`;
+}
+let punto9 = document.getElementById("btnPunto9");
+punto9.addEventListener("click", ejercicio9);
