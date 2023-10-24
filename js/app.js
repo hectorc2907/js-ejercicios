3c8a47b2e02f61390704b52f9f511fa63554ed7b
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-10-24 11:47:55 -0300

    se realiza el maquetado del punto 7 y se comprueba su funcionamiento

--- js/app.js
+++ js/app.js
@@ -47,9 +47,27 @@ function ejercicio6() {
     mensajePunto6.textContent = `El primer numero(${num1}) es mayor que el segundo(${num2})`;
   } else if (num2 > num1) {
     mensajePunto6.textContent = `El segundo numero(${num2}) es mayor que el primero(${num1})`;
-  }else{
+  } else {
     mensajePunto6.textContent = `Ambos numeros comparten el mismo valor(${num1})`;
   }
 }
 var punto6 = document.getElementById("btnPunto6");
 punto6.addEventListener("click", ejercicio6);
+
+function ejercicio7() {
+  var mensajePunto7 = document.getElementById("mensajePunto7");
+  var num1 = parseInt(prompt("Ingrese el Valor Del Numero 1:"));
+  var num2 = parseInt(prompt("Ingrese el Valor Del Numero 2:"));
+  var num3 = parseInt(prompt("Ingrese el Valor Del Numero 3:"));
+  if ((num1 > num2 && num1 > num3) || (num1 > num2 && num2 == num3)) {
+    mensajePunto7.textContent = `El mayor de los 3 numeros ingresados es el primero(${num1})`;
+  } else if ((num2 > num1 && num2 > num3) || (num2 > num1 && num1 == num3)) {
+    mensajePunto7.textContent = `El mayor de los 3 numeros ingresados es el segundo(${num2})`;
+  } else if ((num3 > num1 && num3 > num2) || (num3 > num1 && num1 == num2)) {
+    mensajePunto7.textContent = `El mayor de los 3 numeros ingresados es el tercero(${num3})`;
+  }else if(num1 == num2 && num2 == num3 && num1 == num3){
+    mensajePunto7.textContent = `Los 3 valores son iguales`;
+  }
+}
+var punto7 = document.getElementById("btnPunto7");
+punto7.addEventListener("click", ejercicio7);
