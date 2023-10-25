77bf74ed075de549038df28c982d324bee88cdb2
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-10-25 11:34:44 -0300

    se realiza el agredo del icono de javascript en la pesetaña, se cambia el titulo de la pestaña, se agrega la carpeta img con esa imagen, se eliminan los archivos js que no se utilizan

--- js/ejercicio10.js
+++
@@ -1,6 +0,0 @@
-let numero = parseInt(prompt("Ingrese un numero"));
-if(numero%2==0 || numero%3==0 || numero%5==0 || numero%7==0){
-    document.write("El numero ingresado es divisible en alguno de los siguientes numeros: 2, 3, 5 o 7")
-}else{
-    document.write("El numero ingresado NO es divisible en ninguno de los siguientes numeros: 2, 3, 5 o 7")
-}
--- js/ejercicio9.js
+++
@@ -1,8 +0,0 @@
-let frase = prompt("Escribe una frase");
-let cantidad = frase.length;
-let i;
-for (i = 0; i < cantidad; i++) {
-if (frase.substring(i,1) === "a" || frase.substring(i,1) === "e" || frase.substring(i,1) === "i" || frase.substring(i,1) === "o" || frase.substring(i,1) === "u") {
-document.write(frase.substring(i,1))
-}
-}
